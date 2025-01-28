2e394fb63a0f9decc9f6dbbaad77f191d8b82b19
Moises Navarro <moisalejandro@gmail.com>
2025-01-28 08:06:11 -0600

    10 current  2025-01-28 08:06:08  24.11.20250126.4e96537     6.6.72                          *

--- configuration.nix
+++ configuration.nix
@@ -65,6 +65,17 @@
     alejandra
   ];
 
+  # KDE Connect
+  networking.firewall = rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
