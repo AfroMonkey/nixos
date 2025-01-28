b1d6655baa72e4769e56480a3e08fca7b1abc747
Moises Navarro <moisalejandro@gmail.com>
2025-01-28 07:52:02 -0600

    6 current   2025-01-28 07:52:00  24.11.20250126.4e96537     6.6.72                          *

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -17,12 +13,20 @@
   outputs = {
     self,
     nixpkgs,
+    home-manager,
     ...
   } @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.moy = import ./home.nix;
+        }
       ];
     };
   };
