9373098624a4974a77f2fde240ba36b008365409
Moises Navarro <moisalejandro@gmail.com>
2025-01-29 01:28:16 +0000

    wip

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ set -e
 
 DISK=nvme0n1
 # TODO usar DISK
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /home/nixos/nixos/disko.nix --arg device '"/dev/nvme0n1"'
+# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /home/nixos/nixos/disko.nix --arg device '"/dev/nvme0n1"'
+
+mkdir /mnt/etc
+mv /home/nixos/nixos /mnt/etc/nixos
