057a386bc56dc135673d4fa26b17dd634f592aaf
Moises Navarro <moisalejandro@gmail.com>
2025-01-29 01:38:35 +0000

    wip

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 set -e
 
 DISK=nvme0n1
+HOST=afroframe
 # TODO usar DISK
 # sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /home/nixos/nixos/disko.nix --arg device '"/dev/nvme0n1"'
 
-mkdir /mnt/etc
-mv /home/nixos/nixos /mnt/etc/nixos
+# mkdir /mnt/etc
+# mv /home/nixos/nixos /mnt/etc/nixos
+# cp -r /mnt/etc/nixos /mnt/persist/nixos
+
+nixos-install --root /mnt --flake /mnt/etc/nixos#afroframe
