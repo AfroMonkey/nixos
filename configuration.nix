11a18ad7bf56facb0d6e971caa3e710cfc66082c
Moises Navarro <moisalejandro@gmail.com>
2025-01-30 17:19:39 -0600

    flake

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,6 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
-    ./disko.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A simple NixOS flake";
+  description = "afroflake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
@@ -8,21 +8,11 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    impermanence = {
-      url = "github:nix-community/impermanence";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    home-manager,
     ...
   } @ inputs: {
     nixosConfigurations.afroframe = nixpkgs.lib.nixosSystem {
@@ -30,18 +20,7 @@
       modules = [
         inputs.disko.nixosModules.default
         (import ./disko.nix { device = "/dev/nvme0n1"; })
-
         ./configuration.nix
-
-        inputs.home-manager.nixosModules.default
-        inputs.impermanence.nixosModules.impermanence
-
-        # home-manager.nixosModules.home-manager
-        # {
-        #   home-manager.useGlobalPkgs = true;
-        #   home-manager.useUserPackages = true;
-        #   home-manager.users.moy = import ./home.nix;
-        # }
       ];
     };
   };
