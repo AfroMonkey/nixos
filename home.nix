55825bbc55b0d5a34d441128b1c930b70eb85757
Moises Navarro <moisalejandro@gmail.com>
2025-01-30 17:59:15 -0600

    8 current   2025-01-30 17:59:12  24.11.20250129.59e618d     6.6.74                          *

--- home.nix
+++ home.nix
@@ -23,7 +23,16 @@
     vscode
     warp-terminal
     chromium
+    steam
   ];
+
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  #   platformOptimizations.enable = true;
+  # };
 
   programs.git = {
     enable = true;
