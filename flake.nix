a76b10b2995cbab61d1133d76fb445f49cd1d89a
Moises Navarro <moisalejandro@gmail.com>
2025-02-01 20:58:34 -0600

    6 current   2025-02-01 20:57:07  24.11.20250130.666e1b3  6.6.74                          *

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*";
-    # fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz";
+    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz";
 
     disko = {
       url = "github:nix-community/disko";
