7a407c6fe6f4a16d68699ddf0deadaa59857be51
Moises Navarro <moisalejandro@gmail.com>
2025-02-03 09:31:35 -0600

    25 current  2025-02-03 09:31:30  25.05.20250201.3a22805  6.12.10-zen1                          *

--- configuration.nix
+++ configuration.nix
@@ -68,6 +68,13 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+      Experimental = true;
+    };
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
