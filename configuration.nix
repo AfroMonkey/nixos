47d2e311d80eaf755939944c3cd476ff0785d7c2
Moises Navarro <moisalejandro@gmail.com>
2025-02-06 16:30:48 -0600

    32 current  2025-02-06 16:30:43  25.05.20250204.799ba5b  6.13.1-zen1                           *

--- configuration.nix
+++ configuration.nix
@@ -100,8 +100,21 @@
       ALLOW_USERS = ["moy"];
       TIMELINE_CREATE = true;
       TIMELINE_CLEANUP = true;
+      TIMELINE_LIMIT_HOURLY = 5;
+      TIMELINE_LIMIT_DAILY = 7;
+      TIMELINE_LIMIT_WEEKLY = 3;
+      TIMELINE_LIMIT_MONTHLY = 1;
+      TIMELINE_LIMIT_YEARLY = 1;
+    };
+    log = {
+      SUBVOLUME = "/var/log";
+      ALLOW_USERS = ["moy"];
+      TIMELINE_CREATE = true;
+      TIMELINE_CLEANUP = true;
+      TIMELINE_LIMIT_DAILY = 7;
     };
   };
+  services.snapper.persistentTimer = true;
 
   services.fprintd.enable = true;
 
