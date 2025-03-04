b093e296f773db7b3784964b30ef7364fe92a14f
Moises Navarro <moisalejandro@gmail.com>
2025-03-04 10:22:22 -0600

    54 current  2025-03-04 09:57:48  25.05.20250303.ba487db  6.13.5-zen1                          *

--- configuration.nix
+++ configuration.nix
@@ -155,6 +155,12 @@
     randomizedDelaySec = "45min";
   };
 
+  nix.gc = {
+    automatic = true;
+    randomizedDelaySec = "14m";
+    options = "--delete-older-than 10d";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
